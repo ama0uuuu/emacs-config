b031fbaf3a73ab8e93912354e000dc17e3c6c0af
Sunao Kaneko <sunao.kaneko@protonmail.com>
2024-01-21 08:26:25 +0900

    update

--- flake.nix
+++ flake.nix
@@ -70,9 +70,40 @@
 
         overlayAttrs = import ./overlay.nix {inherit inputs;} final pkgs;
 
-        packages = {
+        packages = rec {
           inherit (pkgs) emacs-config;
+
+          emacs-tmp = let
+            initFile = pkgs.runCommandLocal "init.el" {} ''
+              mkdir -p $out
+              touch $out/init.el
+              for file in ${builtins.concatStringsSep " " pkgs.emacs-config.initFiles}; do
+                cat "$file" >> $out/init.el
+                echo >>$out/init.el
+              done
+            '';
+          in
+            pkgs.writeShellScriptBin "emacs-tmp" ''
+              set +u
+              set -x
+
+              initdir="$(mktemp --tmpdir -d emacs-tmp-XXX)"
+
+              cleanup() {
+               rm -rf "$initdir"
+              }
+               trap cleanup ERR EXIT
+
+               ln -s ${initFile}/init.el "$initdir/init.el"
+
+               ${pkgs.emacs-config}/bin/emacs --init-directory="$initdir" "$@"
+            '';
+          default = emacs-tmp;
         };
+
+	apps = pkgs.emacs-config.makeApps {
+	  lockDirName = "lock";
+	};
 
         formatter = pkgs.alejandra;
       };
