826c6b3e682ceac7ef04951c7a1a4b2db19a7ae3
Sunao Kaneko <sunao.kaneko@protonmail.com>
2024-01-21 06:19:22 +0900

    wrap emacs-pgtk

--- overlay.nix
+++ overlay.nix
@@ -3,7 +3,22 @@
   inherit (inputs.org-babel.overlays.default final prev) tangleOrgBabelFile;
   inherit (inputs.emacs-overlay.overlays.emacs final prev) emacs-pgtk;
   registries = import ./registries.nix inputs;
-  emacsPackage = emacs-pgtk;
+  emacsPackage =
+    prev.runCommand "emacs" {
+      buildInputs = [prev.makeWrapper];
+    } ''
+      mkdir $out
+      ln -s ${prev.emacs-pgtk}/* $out
+      rm $out/bin
+      mkdir $out/bin
+      ln -s ${prev.emacs-pgtk}/bin/* $out/bin
+      rm $out/bin/emacs
+      rm $out/bin/emacsclient
+      makeWrapper ${prev.emacs-pgtk}/bin/emacs $out/bin/emacs \
+        --set GTK_IM_MODULE gtk-im-context-simple
+      makeWrapper ${prev.emacs-pgtk}/bin/emacs $out/bin/emacsclient \
+        --set GTK_IM_MODULE gtk-im-context-simple
+    '';
 in {
   emacs-config = emacsTwist {
     inherit emacsPackage;
